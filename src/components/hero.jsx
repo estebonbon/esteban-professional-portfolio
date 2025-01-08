a32c2ed58ca4d6c1c263f343a14ea1a47d025ee7
estebonbon <estesarmiento1@gmail.com>
2025-01-07 21:45:22 -0500

    Added a technologies section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,5 @@
 import profilepic from "../assets/raviKumarProfile.webp"
+import { HERO_CONTENT } from "../constants"
 
 const Hero = () => {
   return (
@@ -6,10 +7,20 @@ const Hero = () => {
         <div className="flex flex-wrap lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
               <div className="flex justify-center lg:p-8">
-                <img src={profilepic} alt="Ravi Profile Pic"/>
+                <img src={profilepic} alt="Ravi Profile Pic" className="border border-stone-900 rounded-3xl"/>
               </div>
             </div>
-        </div>
+            <div className="w-full lg:w-1/2">
+              <div className="flex flex-col items-center lg:items-start mt-10">
+                <h1 className="pb-2 text-4xl tracking-tighter lg:text-8xl">Esteban Sarmiento</h1>
+                <span className="bg-gradient-to-t from-stone-200 to-stone-900 bg-clip-text text-3xl tracking-tight text-transparent">Frontend Developer</span>
+                <p className="my-2 max-w-lg /* sets a limimi */ py-6 text-xl leading-relaxed tracking-tighter /* leading is for line spacing */">
+                  {HERO_CONTENT}
+                </p>
+                <a href="/" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"> Download Resume </a>
+              </div>
+            </div>
+        </div> {/* Inside the flex container paramaters */}
     </div>
   )
 }
---
+++ src/components/technologies.jsx
@@ -0,0 +1,38 @@
+import { RiReactjsLine } from "react-icons/ri"
+import { SiHtml5, SiCss3, SiJavascript, SiTailwindcss } from "react-icons/si";
+import { FiTerminal } from "react-icons/fi";
+
+
+
+
+
+const Technologies = () => {
+  return (
+    <div className="pb-24">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+          <div className="p-4">
+            <RiReactjsLine className="text-7xl text-cyan-400" />
+          </div>
+          <div className="p-4">
+            <SiHtml5 className="text-7xl text-orange-500" />
+          </div>
+          <div className="p-4"> 
+            <SiJavascript className="text-7xl text-yellow-400" />
+          </div>
+          <div className="p-4">
+            <SiCss3 className="text-7xl text-blue-500" />
+          </div>
+          <div className="p-4">
+            <SiTailwindcss className="text-7xl text-teal-500" />
+          </div>
+          <div className="p-4">
+            <FiTerminal className="text-7xl text-green-500" />
+          </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Technologies
