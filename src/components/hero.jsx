f34a27a8e603c1e0702a0112c65a7a403d563c8f
estebonbon <estesarmiento1@gmail.com>
2025-01-08 20:30:28 -0500

    Added animations to the technolgies section of my webpage

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,23 @@
 import profilepic from "../assets/raviKumarProfile.webp"
 import { HERO_CONTENT } from "../constants"
+import {motion} from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100},
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    }
+  }
+}
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5} }
+}
 
 const Hero = () => {
   return (
@@ -7,18 +25,37 @@ const Hero = () => {
         <div className="flex flex-wrap lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
               <div className="flex justify-center lg:p-8">
-                <img src={profilepic} alt="Ravi Profile Pic" className="border border-stone-900 rounded-3xl"/>
+                <motion.img 
+                src={profilepic} 
+                alt="Ravi Profile Pic" 
+                className="border border-stone-900 rounded-3xl"
+                width={650}
+                height={650}
+                initial={{ x:100, opacity: 0}}
+                animate={{ x: 0, opacity: 1}}
+                transition={{ duration: 1, delay: 1.5}}/>
               </div>
             </div>
             <div className="w-full lg:w-1/2">
-              <div className="flex flex-col items-center lg:items-start mt-10">
-                <h1 className="pb-2 text-4xl tracking-tighter lg:text-8xl">Esteban Sarmiento</h1>
-                <span className="bg-gradient-to-t from-stone-200 to-stone-900 bg-clip-text text-3xl tracking-tight text-transparent">Frontend Developer</span>
-                <p className="my-2 max-w-lg /* sets a limimi */ py-6 text-xl leading-relaxed tracking-tighter /* leading is for line spacing */">
-                  {HERO_CONTENT}
-                </p>
-                <a href="/" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"> Download Resume </a>
-              </div>
+                <motion.div 
+                initial="hidden"
+                animate="visible"
+                variants={containerVariants}
+                className="flex flex-col items-center lg:items-start mt-10">
+                  <motion.h1 
+                  className="pb-2 text-4xl tracking-tighter lg:text-8xl"
+                  variants={childVariants}>Esteban Sarmiento</motion.h1>
+                  <motion.span 
+                  className="bg-gradient-to-t from-stone-200 to-stone-900 bg-clip-text text-3xl tracking-tight text-transparent"
+                  variants={childVariants}>Frontend Developer</motion.span>
+                  <motion.p 
+                  className="my-2 max-w-lg /* sets a limimi */ py-6 text-xl leading-relaxed tracking-tighter /* leading is for line spacing */"
+                  variants={childVariants}>
+                    {HERO_CONTENT}
+                  </motion.p>
+                  <motion.a href="/" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
+                  variants={childVariants}> Download Resume </motion.a>
+                </motion.div>
             </div>
         </div> {/* Inside the flex container paramaters */}
     </div>
--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -1,31 +1,77 @@
 import { RiReactjsLine } from "react-icons/ri"
 import { SiHtml5, SiCss3, SiJavascript, SiTailwindcss } from "react-icons/si";
 import { FiTerminal } from "react-icons/fi";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: {y: -10},
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  }
+})
+
 
 const Technologies = () => {
   return (
     <div className="pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="p-4">
-            <RiReactjsLine className="text-7xl text-cyan-400" />
-          </div>
-          <div className="p-4">
-            <SiHtml5 className="text-7xl text-orange-500" />
-          </div>
-          <div className="p-4"> 
-            <SiJavascript className="text-7xl text-yellow-400" />
-          </div>
-          <div className="p-4">
-            <SiCss3 className="text-7xl text-blue-500" />
-          </div>
-          <div className="p-4">
-            <SiTailwindcss className="text-7xl text-teal-500" />
-          </div>
-          <div className="p-4">
-            <FiTerminal className="text-7xl text-green-500" />
-          </div>
-      </div>
+      <motion.h2 
+          whileInView={{ opacity: 1, y: 0}}
+          initial={{ opacity: 0, y: -100}}
+          transition={{ duration: 1.5 }} 
+          className="my-20 text-center text-4xl">Technologies</motion.h2>
+      <motion.div 
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100}}
+          transition={{ duration: 1.5 }} 
+          className="flex flex-wrap items-center justify-center gap-4">
+              <motion.div 
+                  initial="initial"
+                  animate="animate"
+                  variants={iconVariants(4)}>
+                  <RiReactjsLine className="text-7xl text-cyan-400" />
+              </motion.div>
+              <motion.div 
+                  initial="initial"
+                  animate="animate"
+                  variants={iconVariants(4)}
+                  className="p-4">
+                  <SiHtml5 className="text-7xl text-orange-500" />
+              </motion.div>
+              <motion.div 
+                  initial="initial"
+                  animate="animate"
+                  variants={iconVariants(4)}
+                  className="p-4"> 
+                  <SiJavascript className="text-7xl text-yellow-400" />
+              </motion.div>
+              <motion.div 
+                  initial="initial"
+                  animate="animate"
+                  variants={iconVariants(4)}
+                  className="p-4">
+                  <SiCss3 className="text-7xl text-blue-500" />
+              </motion.div>
+              <motion.div 
+                  initial="initial"
+                  animate="animate"
+                  variants={iconVariants(4)}
+                  className="p-4">
+                  <SiTailwindcss className="text-7xl text-teal-500" />
+              </motion.div>
+              <motion.div 
+                  initial="initial"
+                  animate="animate"
+                  variants={iconVariants(4)}
+                  className="p-4">
+                  <FiTerminal className="text-7xl text-green-500" />
+              </motion.div>
+      </motion.div>
     </div>
   )
 }
