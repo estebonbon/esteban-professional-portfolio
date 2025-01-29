2670985b959514af6694644d3155abc18a6f1dfc
estebonbon <estesarmiento1@gmail.com>
2025-01-29 03:29:45 -0500

    Reformated my contact section, added extra subheadings

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,37 +3,47 @@ import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 const Contact = () => {
   return (
-    <div className="border-t border-stone-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
-      <div className="lg:flex lg:items-center lg:justify-between lg:mx-auto lg:max-w-4xl lg:text-left text-center">
-        {/* Left Content */}
-        <div>
-          <p className="my-4 lg:my-2" aria-label="location"> <span className="ml-2"> &#128205;</span> Location: {CONTACT.located}</p>
-          <p className="my-4 lg:my-2">
+    <div className="border-t border-stone-900  pb-5 lg:pb-20">
+      <h2 className="lg:my-20 my-10 text-center text-4xl">Get in Touch</h2>
+
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start lg:mx-auto lg:max-w-4xl lg:text-left text-center">
+        {/* Left Content Contact Me*/}
+        <div className="lg:flex-1">
+          <h3 className="text-center text-3xl text-cyan-800">Contact Me</h3>
+          <p className="my-4 lg:my-6 py-3 ">
             <a href="mailto:estebancodecity@gmail.com" 
-              className="border-b border-transparent hover:border-stone-400">
-              <span className="mr-2" role="img" aria-label="Email">&#x1F4E7;</span> Mail: {CONTACT.email}
+              className="border-b border-transparent hover:border-stone-400 text-[17px]">
+              <span className="" role="img" aria-label="Email">&#x1F4E7;</span> Mail: {CONTACT.email}
             </a>
           </p>        
         </div> {/* Close Left Content */}        
-        {/* Right Content */}
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-          <a
-            href="https://www.linkedin.com/in/esteban-sarmiento/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Linkedin Profile">
-             <FaLinkedin className="text-5xl mr-10 hover:text-[#c5a358ce]"/> 
-          </a>
-          <a
-            href="https://github.com/estebonbon"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Github">
-            <FaGithub className="text-5xl hover:text-[#c5a358ce]"/>
-          </a>
-        </div>
+        {/* Middle Content Follow Me */}
+        <div className="lg:flex-1 text-center">
+          <h3 className="text-center text-3xl text-cyan-800 ">Follow Me</h3>
+          <div className="mx-8 my-4 lg:my-6 py-3 flex items-center justify-center gap-4 text-2xl">
+            <a
+              href="https://www.linkedin.com/in/esteban-sarmiento/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Linkedin Profile">
+              <FaLinkedin className="text-4xl mr-10 hover:text-[#c5a358ce]"/> 
+            </a>
+            <a
+              href="https://github.com/estebonbon"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Github">
+              <FaGithub className="text-4xl hover:text-[#c5a358ce]"/>
+            </a>
+          </div> 
+        </div> {/* Close middle content */}
+        <div className="lg:flex-1"> {/* Right content Meet Me */}
+        <h3 className="text-center text-3xl  text-cyan-800">Meet Me</h3>
+          <p className="my-4 lg:my-6 mr-2 py-3 lg:text-center text-[17px]" aria-label="location"> <span className=""> &#128205;</span> Location: {CONTACT.located}
+          </p>        
+        </div> {/* Close Right Content */}  
       </div>
+      <p className="text-[14px] text-center mt-10 lg:mt-20"> Copyright 2025 | <span className="">Esteban Sarmiento</span></p>
     </div>
   );
 };
